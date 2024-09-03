279d66b1af5ae5b09c9c2228f77f5a6cb87adef5
Christophe Calvet <onlyatest@mytest.com>
2024-09-03 16:09:34 +0200

    done

--- server/app.js
+++ server/app.js
@@ -25,6 +25,44 @@ const express = require('express');
 const app = express();
 
 // Your code here
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log('Request Body:', req.body);
+  next();
+});
+
+
+app.get('/artists', (req, res) => {
+  const allArtists = getAllArtists()
+  res.json(allArtists)
+
+});
+
+app.post('/artists', (req, res) => {
+ const newArtist =  addArtist(req.body);
+  res.status(201);
+  //res.set('Content-Type', 'application/json')
+  res.json(newArtist)
+
+});
+
+app.get('/artists/latest', (req, res) => {
+  const latestArtist = getLatestArtist()
+  res.json(latestArtist)
+});
+
+app.get('/artists/latest/albums', (req, res) => {
+  const albumsForLatestArtist = getAlbumsForLatestArtist()
+  res.json(albumsForLatestArtist)
+});
+
+app.get('/artists/*', (req, res) => {
+  const artistId = req.url.split("/")[2]
+  res.json(getArtistByArtistId(artistId))
+
+});
+
+
 
 // DO NOT MODIFY
 if (require.main === module) {
